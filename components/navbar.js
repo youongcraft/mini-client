a1cb889bc0a4f78ea13230e5d4e0b0cd89018cd3
youongcraft <kantitat_hwanchuen@hotmail.co.th>
2021-04-21 21:37:36 +0700

    pull 2

---
+++ components/navbar.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+const Navbar = () => (
+    <div>
+        <Link href="/"><a> Home </a></Link> |
+        <Link href="/register"><a> Register </a></Link>  | 
+        <Link href="/login"><a> Login </a></Link> |
+        <Link href="/profile"><a> Profile </a></Link> | 
+        <Link href="/editstudents"><a> <b>Edit Students </b> </a></Link> |
+        <Link href="/logout"><a> Logout </a></Link>
+        
+    </div>
+)
+
+export default Navbar
